a06624f1fffb5f506485e7594c4572f2b521ac74
Bogdan_s <bogdans20o62002@gmail.com>
2023-05-17 22:16:00 +0300

    .

--- index.js
+++ index.js
@@ -2,8 +2,18 @@ const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
 
 const app = express();
-app.use(express.json());
+app.use((req, res, next) =>{
+  res.setHeader('Access-Control-Allow-Origin', 'https://b-ogdan-s.github.io');
+  // Другие заголовки CORS, если необходимо
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 const port = 3000;
+
+app.listen(port, () => {
+  console.log('Сервер запущен на порту 3000');
+});
 
 // Парсер для обработки данных запроса в формате JSON
 
