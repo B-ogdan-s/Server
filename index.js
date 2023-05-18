17e5dc1b52711fac0be74f3a7b8ad241638fad3d
Bogdan_s <bogdans20o62002@gmail.com>
2023-05-18 11:02:33 +0300

    ,

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const app = express();
 app.use((req, res, next) =>{
   res.setHeader('Access-Control-Allow-Origin', 'https://b-ogdan-s.github.io');
   // Другие заголовки CORS, если необходимо
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  //res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  //res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
 const port = 3000;
