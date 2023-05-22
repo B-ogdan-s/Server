aa0c791db6ca37e67b22928eb6e8b16402963848
Bogdan_s <bogdans20o62002@gmail.com>
2023-05-22 21:46:31 +0300

    .

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
 
 const app = express();
-app.use(express.json());
+app.use(express.json({limit: '50mb'}))
 
 app.use((req, res, next) =>{
   res.setHeader('Access-Control-Allow-Origin', 'https://b-ogdan-s.github.io');
